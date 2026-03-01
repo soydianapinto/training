6de6de17a451f524a4486923ab56b50d5394655e
Diana Pinto <diana.apolinar@hotmail.com>
2026-03-01 14:40:53 -0600

    Inverted binary tree

--- ts/InvertBinaryTree.ts
+++ ts/InvertBinaryTree.ts
@@ -1,5 +1,7 @@
 // Given the root of a binary tree, invert the tree, and return its root.
 
+// Recursivity.
+
 // Definition for a binary tree node.
  class TreeNode {
      val: number
@@ -13,5 +15,24 @@
  }
 
 function invertTree(root: TreeNode | null): TreeNode | null {
-    return null;
+    // Is there a node?
+    if (root === null) {
+        return null;
+    }
+
+    // We do the swap
+    let temp = root?.left;
+    root.left = root?.right;
+    root.right = temp;
+
+    // Call for the rest of the children
+    invertTree(root.left);
+    invertTree(root.right);
+
+    // Return the inverted root
+    return root;
 };
+
+let root = new TreeNode(2, new TreeNode(6, new TreeNode(7), new TreeNode(9)), new TreeNode(4, new TreeNode(5), new TreeNode(1)));
+console.log(root);
+console.log(invertTree(root));
---
+++ ts/validAnagram.ts
@@ -0,0 +1,5 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+function isAnagram(s: string, t: string): boolean {
+    
+};
