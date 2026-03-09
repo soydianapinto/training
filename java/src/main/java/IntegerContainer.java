b765c7e3569ac7dc08701d4cc138e94cddea7c64
Diana Pinto <diana.apolinar@hotmail.com>
2026-03-09 16:18:56 -0600

    CodeSignal test

--- java/src/main/java/IntegerContainer.java
+++ java/src/main/java/IntegerContainer.java
@@ -5,6 +5,7 @@ public class IntegerContainer implements IntegerContainerInterface {
 
     private ArrayList<Integer> container;
 
+    // Constructor, output
     public IntegerContainer() {
         this.container = new ArrayList<>();
     }
@@ -22,17 +23,37 @@ public class IntegerContainer implements IntegerContainerInterface {
 
     @Override
     public Integer getMedian() {
+        // If its empty return null
         if (this.container.isEmpty()) {
             return null;
         }
 
+        // copy of new output
         ArrayList<Integer> copy = new ArrayList<>(this.container);
 
+        // iterate (sorting)
         Collections.sort(copy);
 
+        // get middle
         int middleIndex = (copy.size() - 1) / 2;
 
+        // return
         return copy.get(middleIndex);
+    }
+
+    public static void main(String[] args) {
+        IntegerContainer integerContainer = new IntegerContainer();
+        System.out.println();
+        System.out.println(integerContainer.add(2));
+        System.out.println(integerContainer.add(3)); // removed
+        System.out.println(integerContainer.add(10));
+        System.out.println(integerContainer.add(15));
+        System.out.println(integerContainer.add(30));
+        System.out.println(integerContainer.add(40));
+        System.out.println();
+        System.out.println(integerContainer.delete(3));
+        System.out.println();
+        System.out.println(integerContainer.getMedian());
     }
     
 }
