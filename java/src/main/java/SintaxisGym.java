66331fa4209b320a6b907303dab2d0d7333015fc
Diana Pinto <diana.apolinar@hotmail.com>
2026-03-09 16:11:16 -0600

    Practicing sintaxis for java

--- java/src/main/java/SintaxisGym.java
+++ java/src/main/java/SintaxisGym.java
@@ -7,11 +7,11 @@ public class SintaxisGym {
      public static void main(String[] args) {
         int[] oddFilterInput = {1, 2, 3, 4};
         String textInverterString = "textineedtoinverter";
-        int[] countFrequenciesInput = {10, 20, 30, 40};
+        int[] countFrequenciesInput = {10, 20, 30, 40, 10, 15, 30};
 
         System.out.println();
         System.out.println("Odd Filter input: " + Arrays.toString(oddFilterInput));
-        System.out.println("Odd Filter output: " + oddFilter(oddFilterInput));
+        System.out.println("Odd Filter output: " + evenFilter(oddFilterInput));
         System.out.println();
         System.out.println("Text Inverter input: " + textInverterString);
         System.out.println("Text Inverter output: " + textInverter(textInverterString));
@@ -20,25 +20,69 @@ public class SintaxisGym {
         System.out.println("Count Frequencies output: " + countFrequencies(countFrequenciesInput));
      }
 
-    public static ArrayList<Integer> oddFilter(int[] numbers) {
-        // Create ArrayList (why? )
+    public static ArrayList<Integer> evenFilter(int[] numbers) {
+        // Create ArrayList (why ArrayList? porque no sabes cuántos pares va a contener el arreglo de salida
+        // un simple int[] tiene que especificar cuánta longitud debe haber en el arreglo, no es útil)
+        // Number Names: "Even" has 4 letters (even, pares), and "Odd" has 3 letters (odd, impares).
+
+        // output
         ArrayList<Integer> output = new ArrayList<>();
+
+        // iterate
         for (int i : numbers) {
-            if (i % 2 != 0) {
-                output.add(numbers[i]);
+            if (i % 2 == 0) {
+                output.add(i);
             }
         }
+
+        // return
         return output;
     }
 
     public static String textInverter(String text) {
-        // Code
-        return "";
+        // La única diferencia entre usar el bucle normal y el bucle invertido es dónde 
+        // se coloca la nueva letra al momento de pegarla. Es un simple truco de concatenación.
+        // normal for: izq a der. textInverted = text.charAt(i) + textInverted;
+        // interved for: der a izq. textInverted = textInverted + text.charAt(i);
+
+        // output
+        String textInverted = "";
+
+        // iterate
+        for (int i = text.length() - 1; i >= 0 ; i--) {
+            textInverted += text.charAt(i);
+        }
+
+        // return
+        return textInverted;
     }
     
     public static HashMap<Integer, Integer> countFrequencies(int[] numbers) {
-        // Code
-        return new HashMap<>();
+        // {1, 2, 3, 2} = i
+        // 
+
+        // output
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        // iterate
+        // old way
+        /*
+        for(int i: numbers) {
+            int frequency = 0;
+            if (hashMap.containsKey(i)) {
+                frequency = hashMap.get(i);
+                hashMap.replace(i, ++frequency);
+            } else {
+                hashMap.put(i, ++frequency);
+            }
+        }
+        */
+        for(int num: numbers) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+
+        // return
+        return hashMap;
     }
 }
 
